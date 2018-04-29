d08367b9feb725826e8ad64150a48edc1fb87d29
mattshirtliffe <matt.shirtliffe@gmail.com>
2018-04-29 20:26:01 +0100

    slug view

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='blog'),
-    path('<int:post_id>/', views.post, name='post')
+    path('<int:post_id>/', views.post, name='post'),
+    path('slug/<str:slug>/', views.slug, name='slug')
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
 
 from .models import Post
 
@@ -10,3 +11,9 @@ def index(request):
 def post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/post.html',{'post':post})
+
+def slug(request, slug):
+    post = Post.objects.filter(slug=slug).first()
+    if post:
+        return render(request, 'blog/post.html',{'post':post})
+    return HttpResponseNotFound('<h1>Page not found</h1>')
